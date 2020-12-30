f37b0c7431a0ab1830df90d6f66d2949a5a2502b
FacundoPonce <mrfacu125@gmail.com>
2020-12-30 16:24:59 -0300

    Created scene with ball and kagels.

---
+++ Assets/Scripts/BallShoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShoot : MonoBehaviour
+{
+    public float ballSpeed;
+    const int speedLimit = 55;
+    public Rigidbody ball;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(ballSpeed <= speedLimit)
+                ballSpeed += 10 * Time.deltaTime;
+        }
+        else
+        {
+            ball.transform.position += (Vector3.forward * ballSpeed) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/PlaceBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBall : MonoBehaviour
+{
+    public Rigidbody ball;
+    private float speedMove;
+
+    void Start()
+    {
+        speedMove = 10;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            ball.transform.position += (Vector3.left * speedMove) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ball.transform.position += (Vector3.right * speedMove) * Time.deltaTime;
+        }
+    }
+}
